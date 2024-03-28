8bb2ce35d0dbb589351ee6df646d4d34d0265c51
Qetsia Nkulu <qetsiankulu@gmail.com>
2024-03-27 17:42:42 -0700

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Qetsia Nkulu  on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
